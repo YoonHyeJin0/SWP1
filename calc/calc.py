194fbd8c9d6098a54c863f7e29f2002b00f50c2a
YoonHyeJin0 <hyejin91655@gmail.com>
2020-06-18 16:00:50 +0900

    Issue #5 Modify initiate value of sum and mul : 0 -> -1

--- calc/calc.py
+++ calc/calc.py
@@ -5,8 +5,8 @@ def application(environ, start_response) :
         d = parse_qs(environ['QUERY_STRING'])
         a = d.get('a', [''])[0]
         b = d.get('b', [''])[0]
-        sum = 0
-        mul = 0
+        sum = -1
+        mul = -1
         if '' not in [a, b]:
                 a, b = int(a), int(b)
                 sum = (a+b)
